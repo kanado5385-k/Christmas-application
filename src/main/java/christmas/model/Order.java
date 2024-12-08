d9d28ed169977ae641f4645042e19c5004fd1179
이윤빈 <kanado20000@naver.com>
2024-12-08 16:21:21 +0900

    :sparkles: feat: 사용지 주문을 관리하는 Order 클래스 추가

---
+++ src/main/java/christmas/model/Order.java
@@ -0,0 +1,12 @@
+package christmas.model;
+
+import java.util.Map;
+
+public class Order {
+    private final Integer date;
+    private final Map<String,Integer> menu;
+
+    public Order(String date, String menu) {
+        
+    }
+}
---
+++ src/main/java/christmas/utilities/Parser.java
@@ -0,0 +1,5 @@
+package christmas.utilities;
+
+public class Parser {
+    
+}
